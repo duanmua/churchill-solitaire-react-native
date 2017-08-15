b489fbe3de4cf5de25b8f9b159b2f41274e37fd0
Alan Duanmu <alan.duanmu@gmail.com>
2017-08-14 17:43:33 -0700

    Move deck initialization code to own file

--- src/reducers/BoardReducer.js
+++ src/reducers/BoardReducer.js
@@ -13,60 +13,10 @@ import {
   RED
 } from '../CardTypes';
 
+import { shuffle, populate, dealLaneOfDepth, dealLanes } from '../DeckCreator';
+
 import { SCREEN_WIDTH } from '../LayoutConstants';
 
-const shuffle = (array) => {
-  var i = 0
-    , j = 0
-    , temp = null;
-
-  for (i = array.length - 1; i > 0; i -= 1) {
-    j = Math.floor(Math.random() * (i + 1));
-    temp = array[i];
-    array[i] = array[j];
-    array[j] = temp;
-  }
-};
-
-const populate = (board) => {
-  var i = 0;
-  Array(2).fill().forEach(() => {
-    [SPADES, HEARTS, DIAMONDS, CLUBS].forEach((suit) => {
-      ['A', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K'].forEach((value) => {
-        board.deck.push({
-          id: i,
-          value: value,
-          suit: suit,
-          color: suit === SPADES || suit === CLUBS ? BLACK : RED,
-          shown: false,
-          draggable: false
-        })
-      });
-    });
-  });
-};
-
-const dealLaneOfDepth = (num, deck) => {
-  return Array(num).fill().map((_, i) => {
-    var card = deck.shift();
-    if (i===0) {
-      card.shown = true;
-      card.draggable = true;
-    }
-    return card;
-  });
-};
-
-const dealLanes = (board) => {
-  var lanes = [];
-  for (i = 1; i<=5; i++) {
-    lanes.push(dealLaneOfDepth(i, board.deck));
-  }
-  for (j = 5; j>0; j--) {
-    lanes.push(dealLaneOfDepth(j, board.deck));
-  }
-  return lanes;
-};
 
 const INITIAL_STATE = (() => {
   var board = {};
