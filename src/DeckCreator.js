1fd14df387007a0d697bc460e9a751e86508d5f9
Alan Duanmu <alan.duanmu@gmail.com>
2017-08-14 18:08:32 -0700

    Forgot to commit DeckCreator

---
+++ src/DeckCreator.js
@@ -0,0 +1,63 @@
+import {
+  SPADES,
+  HEARTS,
+  DIAMONDS,
+  CLUBS,
+  BLACK,
+  RED
+} from './CardTypes';
+
+export const CARD_VALUES = ['A', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K'];
+
+export const shuffle = (array) => {
+  var i = 0
+    , j = 0
+    , temp = null;
+
+  for (i = array.length - 1; i > 0; i -= 1) {
+    j = Math.floor(Math.random() * (i + 1));
+    temp = array[i];
+    array[i] = array[j];
+    array[j] = temp;
+  }
+};
+
+export const populate = (board) => {
+  var i = 0;
+  Array(2).fill().forEach(() => {
+    [SPADES, HEARTS, DIAMONDS, CLUBS].forEach((suit) => {
+      CARD_VALUES.forEach((value) => {
+        board.deck.push({
+          id: i,
+          value: value,
+          suit: suit,
+          color: suit === SPADES || suit === CLUBS ? BLACK : RED,
+          shown: false,
+          draggable: false
+        })
+      });
+    });
+  });
+};
+
+export const dealLaneOfDepth = (num, deck) => {
+  return Array(num).fill().map((_, i) => {
+    var card = deck.shift();
+    if (i===0) {
+      card.shown = true;
+      card.draggable = true;
+    }
+    return card;
+  });
+};
+
+export const dealLanes = (board) => {
+  var lanes = [];
+  for (i = 1; i<=5; i++) {
+    lanes.push(dealLaneOfDepth(i, board.deck));
+  }
+  for (j = 5; j>0; j--) {
+    lanes.push(dealLaneOfDepth(j, board.deck));
+  }
+  return lanes;
+};
