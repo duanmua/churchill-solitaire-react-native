95d4f0281dd4bd1c411bc22c9bb91cd8d9aa71d8
Alan Duanmu <alan.duanmu@gmail.com>
2017-08-14 18:08:13 -0700

    Movement logic correct

--- src/reducers/BoardReducer.js
+++ src/reducers/BoardReducer.js
@@ -13,7 +13,7 @@ import {
   RED
 } from '../CardTypes';
 
-import { shuffle, populate, dealLaneOfDepth, dealLanes } from '../DeckCreator';
+import { CARD_VALUES, shuffle, populate, dealLaneOfDepth, dealLanes } from '../DeckCreator';
 
 import { SCREEN_WIDTH } from '../LayoutConstants';
 
@@ -29,28 +29,57 @@ const INITIAL_STATE = (() => {
 
 const MAX_LENGTH = 99999;
 
+const canMove = (cards, target) => {
+  if (target.indexOf(cards[0]) > -1) {
+    return false;
+  }
+  let top = cards[cards.length-1];
+  if (target.length === 0) {
+    return top.value === 'K';
+  }
+  let bottom = target[0];
+
+  if (top.color === bottom.color) {
+    return false;
+  }
+  if (top.value === CARD_VALUES[CARD_VALUES.indexOf(bottom.value)-1]) {
+    return true;
+  }
+};
+
+const cardMoved = (state, payload) => {
+  const { lane, index, endX } = payload;
+
+  var cards = state.lanes[lane].slice(0, index+1);
+
+  var spliceConfig = [0, index+2];
+
+  const target = Math.floor(endX / (SCREEN_WIDTH / 10));
+
+  if (canMove(cards, state.lanes[target])) {
+    const newTop = state.lanes[lane][index+1];
+
+    if (newTop !== undefined) {
+      newTop.shown = true;
+      newTop.draggable = true;
+      spliceConfig.push(newTop);
+    }
+
+    return update(state, {lanes: {
+                            [lane]: {
+                              $splice: [spliceConfig],
+                            },
+                            [target]: {$unshift: cards}
+                          }});
+  }
+  return state;
+};
+
 export default (state = INITIAL_STATE, action) => {
   switch (action.type) {
     case CARD_MOVED:
-      const { lane, index, endX } = action.payload;
-      var cards = state.lanes[lane].slice(0, index+1);
-      var spliceConfig = [0, index+2];
-      const newTop = state.lanes[lane][index+1];
-      if (newTop !== undefined) {
-        newTop.shown = true;
-        newTop.draggable = true;
-        spliceConfig.push(newTop);
-      }
-      const target = Math.floor(endX / (SCREEN_WIDTH / 10));
-      if (lane === target) {
-        return state;
-      }
-      return update(state, {lanes: {
-                              [lane]: {
-                                $splice: [spliceConfig],
-                              },
-                              [target]: {$unshift: cards}
-                            }});
+      return cardMoved(state, action.payload);
+
     case DECK_TAPPED:
       var cards = state.deck.slice(0, 10);
       return update(state, {
